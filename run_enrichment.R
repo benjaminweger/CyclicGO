58dfa3c8ab87ca1b3eb21344b1af796c763be0fb
Jake Yeung <jake.yeung@epfl.ch>
2018-08-14 11:23:13 +0200

    Try with 2 loops because first loop works, second loop breaks

--- run_enrichment.R
+++ run_enrichment.R
@@ -14,14 +14,11 @@ library(topGO)
 library(org.Mm.eg.db)
 library(GenomicRanges)
 
-
 data(genes.fg.all)
 data(genes.bg)
 
-
 GOterms <- c("GO:0006260", "GO:0042254", "GO:0032868", "GO:0043434", "GO:0046326", "GO:0090526", "GO:0006111",
              "GO:0005977", "GO:0006096", "GO:0006111", "GO:0006110")
-
-enrichment <- RunEnrichment(genes.fg.all, genes.bg, tstarts = c(15), GOterms = GOterms, ncores = 1)
+enrichment <- RunEnrichment(genes.fg.all, genes.bg, tstarts = c(15, 5), GOterms = GOterms, ncores = 1)
 
 print(enrichment)
